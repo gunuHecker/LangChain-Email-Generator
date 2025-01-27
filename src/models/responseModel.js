da675a5ad21c98ae7bb2e13f9c9a8a781bcfaa4a
Codesorcer <Rudraprataps7789@gmail.com>
2025-01-28 00:05:49 +0530

    connect db and create models

---
+++ src/models/responseModel.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const responseSchema = new mongoose.Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: [true, "No user found"],
+  },
+  recipientName: {
+    type: String,
+    required: [true, "Please provide recipient name"],
+  },
+  purpose: {
+    type: String,
+    required: [true, "Please provide mail purpose"],
+    enum: ["Meeting Request", "Follow Up", "Thank You"],
+  },
+  keyPoints: {
+    type: String,
+    required : [true, "Please provide key points of the mail"],
+  },
+  mail: {
+    type: String,
+  },
+});
+
+const Response = mongoose.models.responses || mongoose.model("responses", responseSchema);
+
+export default Response;
