39f5d5ef0b616ee47dc6f7f248725cf5b696f43b
Codesorcer <Rudraprataps7789@gmail.com>
2025-02-04 06:38:10 +0530

    LangSmith integration

--- src/app/api/generate-email/route.js
+++ src/app/api/generate-email/route.js
@@ -2,19 +2,23 @@ import { connect } from "@/dbConfig/dbConfig";
 import Response from "@/models/responseModel";
 import { NextResponse } from "next/server";
 import { HfInference } from "@huggingface/inference";
+import { HuggingFaceInference } from "@langchain/community/llms/hf";
+//import { RunTree, Client } from "@langsmith/client"; // Import LangSmith
 import { cookies } from "next/headers";
 import User from "@/models/userModel";
 
 connect();
 
 const HUGGING_FACE_API_KEY = process.env.HUGGING_FACE_API_KEY;
+const LANGSMITH_API_KEY = process.env.LANGCHAIN_API_KEY;
 
 export async function POST(request) {
   try {
     const { recipientName, emailPurpose, keyPoints } = await request.json();
 
     // Access cookies safely using Next.js API
-    const userId = await cookies().get("userId")?.value;
+    const cookieStore = await cookies();
+    const userId = cookieStore.get("userId")?.value;
 
     if (!userId) {
       console.error("User ID is missing in the request.");
@@ -35,8 +39,9 @@ export async function POST(request) {
 
       fullname = loggedInUser.username;
       emailId = loggedInUser.email;
-      console.log("fullname: ", fullname);
-      console.log("emailId: ", emailId);
+      // console.log("fullname: ", fullname);
+      // console.log("emailId: ", emailId);
+      // console.log("userID: ", userId);
     } catch (error) {
       console.error("An error occurred while fetching the user:", error);
     }
@@ -46,42 +51,83 @@ export async function POST(request) {
 
     // Calling Hugging Face Inference API with Qwen2.5-Coder-32B-Instruct
 
-    const client = new HfInference(HUGGING_FACE_API_KEY);
-    const chatCompletion = await client.chatCompletion({
+    // const client = new HfInference(HUGGING_FACE_API_KEY);
+    // const chatCompletion = await client.chatCompletion({
+    //   model: "Qwen/Qwen2.5-Coder-32B-Instruct",
+    //   messages: [
+    //     {
+    //       role: "user",
+    //       content: prompt,
+    //     },
+    //   ],
+    //   provider: "hf-inference",
+    //   max_tokens: 500,
+    // });
+
+    // if (
+    //   chatCompletion &&
+    //   chatCompletion.choices &&
+    //   chatCompletion.choices.length > 0
+    // ) {
+    //   //const email = chatCompletion.choices[0].message.content;
+
+    //   // const newResponse = new Response({
+    //   //   userId: request.userId,
+    //   //   recipientName: recipientName,
+    //   //   purpose: emailPurpose,
+    //   //   keyPoints: keyPoints,
+    //   //   mail: email,
+    //   // });
+
+    //  // const savedResponse = await newResponse.save();
+    //   return NextResponse.json({ email });
+    // } else {
+    //   throw new Error("Failed to generate email");
+    // }
+
+    // if (
+    //   res &&
+    //   res.choices &&
+    //   res.choices.length > 0
+    // ) {
+    //   const email = res.choices[0].message.content;
+
+    //   // const newResponse = new Response({
+    //   //   userId: request.userId,
+    //   //   recipientName: recipientName,
+    //   //   purpose: emailPurpose,
+    //   //   keyPoints: keyPoints,
+    //   //   mail: email,
+    //   // });
+
+    //  // const savedResponse = await newResponse.save();
+    //   return NextResponse.json({ email });
+    // } else {
+    //   throw new Error("Failed to generate email");
+    // }
+
+    // Integrating LangSmith is not possible in javascript in HfInference so I had to switch to HuggingFaceInference
+
+    const model = new HuggingFaceInference({
       model: "Qwen/Qwen2.5-Coder-32B-Instruct",
-      messages: [
-        {
-          role: "user",
-          content: prompt,
-        },
-      ],
-      provider: "hf-inference",
-      max_tokens: 500,
+      apiKey: HUGGING_FACE_API_KEY, // In Node.js defaults to process.env.HUGGINGFACEHUB_API_KEY
+    });
+    const res = await model.invoke(prompt);
+    console.log({ res });
+
+    const email = res;
+
+    const newResponse = new Response({
+      userId: request.userId,
+      recipientName: recipientName,
+      purpose: emailPurpose,
+      keyPoints: keyPoints,
+      mail: email,
     });
 
-    if (
-      chatCompletion &&
-      chatCompletion.choices &&
-      chatCompletion.choices.length > 0
-    ) {
-      const email = chatCompletion.choices[0].message.content;
-
-      const newResponse = new Response({
-        userId: request.userId,
-        recipientName: recipientName,
-        purpose: emailPurpose,
-        keyPoints: keyPoints,
-        mail: email,
-      });
-
-      const savedResponse = await newResponse.save();
-
-      return NextResponse.json({ email });
-    } else {
-      throw new Error("Failed to generate email");
-    }
-  }
-  catch (error) {
+    const savedResponse = await newResponse.save();
+    return NextResponse.json({ email });
+  } catch (error) {
     console.error("Error:", error.message);
     return NextResponse.json(
       { error: "Failed to generate email" },
